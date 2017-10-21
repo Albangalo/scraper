fc98c951689f79d6740ab4d8cd98954ca6e36945
Albangalo <Albert.chen.nz@gmail.com>
2017-10-21 19:48:18 +1100

    prints title of unimelb html

--- src/org/scraper/Main.java
+++ src/org/scraper/Main.java
@@ -1,8 +1,22 @@
 package org.scraper;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        try {
+            Document doc = Jsoup.connect("https://handbook.unimelb.edu.au/2017/subjects/").get();
+            String sub = doc.title();
+            System.out.println("title of html is: " + sub);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
     }
 }
