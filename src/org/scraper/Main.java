edd5bab8ed479207b0a2fe3ee659c98d535b6513
Albangalo <Albert.chen.nz@gmail.com>
2017-11-13 02:25:46 +1100

    refactored subject list scraping, list prints to csv

--- src/org/scraper/Main.java
+++ src/org/scraper/Main.java
@@ -5,51 +5,27 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException{
         Document doc;
         String title;
 
-        // Prints Hello World
-        System.out.println("Hello, World!");
+        SubjectListScraper subScraper = SubjectListScraper.getScraper();
+        subScraper.writeSubjectsToCSV();
 
         // Creates an HTML as String and parses into document
-        String html = "<html><head><title>First parse</title></head>"
+        /*String html = "<html><head><title>First parse</title></head>"
                 + "<body><p>Parsed HTML into a doc.</p></body></html>";
         doc = Jsoup.parse(html);
-        System.out.println("something: " + doc);
-
-        // Prints title of unimelb subject page
-        try {
-            doc = Jsoup.connect("https://handbook.unimelb.edu.au/2017/subjects/").get();
-            title = doc.title();
-
-            System.out.println("title of html is: " + title);
-            //System.out.println(doc);
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-
-
-        // Prints link and name of each link in 2018 subjects page
-        String subURL = "http://handbook.unimelb.edu.au/subjects/";
-        String subsURL = "https://handbook.unimelb.edu.au/subjects?page=";
-
-        // get number of last page
-        int lastpage = getLastPage(subURL);
-        System.out.println("last page is: " + lastpage);
-
-        // print contents of each page
-        for (int i = 1; i <= lastpage; i++){
-            printSubs(subsURL + i);
-        }
-
-
+        System.out.println("something: " + doc);*/
 
         // Prints link and name of each link in 2016 archived subjects page
         /*try {
@@ -67,7 +43,7 @@ public class Main {
         }*/
 
         // Prints meta keywords and description of javatpoint homepage
-        try{
+        /*try{
             doc = Jsoup.connect("http://www.javatpoint.com").get();
             String keywords = doc.select("meta[name=keywords]").first().attr("content");
             System.out.println("Meta keywords : " + keywords);
@@ -76,55 +52,10 @@ public class Main {
         }
         catch (IOException e){
             e.printStackTrace();
-        }
+        }*/
 
 
     }
 
-    static void printSubs(String url){
-        Document doc;
-        try {
-            doc = Jsoup.connect(url).get();
 
-            Elements links = doc.select("a[href]");
-            for (Element link : links){
-                String input = link.text();
-                if(input.matches(".*[A-Z]{4}[0-9]{5}$")){
-                    String s = (input.substring(link.text().length() - 9));
-                    System.out.println("\nlink : " + link.attr("href"));
-                    System.out.println("text : " + s);
-                }
-            }
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-    }
-
-    // gets the last page of the handbook and returns as integer
-    static int getLastPage(String url){
-        Document doc;
-        Pattern lastPagePattern = Pattern.compile("page=(.*?)$");
-        int last = 1;
-
-        try {
-            doc = Jsoup.connect(url).get();
-            Elements links = doc.select("a[href]");
-            for (Element link : links){
-                String input = link.text();
-                    if (input.matches(".*Last.*")){
-                        String l = link.attr("href");
-                        Matcher m = lastPagePattern.matcher(l);
-                        while (m.find()) {
-                            String s1 = m.group(1);
-                            // s1 should now contain the page number as a string
-                            last = Integer.parseInt(s1);
-                        }
-                    }
-            }
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-
-        return last;
-    }
 }
---
+++ src/org/scraper/SubjectListScraper.java
@@ -0,0 +1,109 @@
+package org.scraper;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SubjectListScraper {
+
+    private static SubjectListScraper scraper;
+
+    private SubjectListScraper(){
+    }
+
+    public static SubjectListScraper getScraper(){
+        if (scraper == null){
+            scraper = new SubjectListScraper();
+        }
+        return scraper;
+    }
+
+    public void writeSubjectsToCSV() throws FileNotFoundException {
+        // Prints link and name of each link in 2018 subjects page
+        String subURL = "https://handbook.unimelb.edu.au";
+        String subjectURLext = "/subjects/";
+        String subURLpageExt = "/subjects?page=";
+
+        // get number of last page
+        int lastpage = getLastPage(subURL + subjectURLext);
+        System.out.println("last page is: " + lastpage);
+
+        // writes subjects of first page to csv
+        PrintWriter pw = new PrintWriter(new File("subjects.csv"));
+        StringBuilder sb = new StringBuilder();
+        sb.append("code");
+        sb.append(',');
+        sb.append("link");
+        sb.append("\n");
+        // print contents of each page
+        for (int i = 1; i <= lastpage; i++){
+            printSubs(subURL, subURLpageExt + i, sb);
+        }
+        pw.write(sb.toString());
+        pw.close();
+        System.out.println("printed to subject.csv");
+
+
+    }
+
+    private static StringBuilder printSubs(String url, String page, StringBuilder sb){
+        Document doc;
+        StringBuilder newSB = sb;
+        try {
+            doc = Jsoup.connect(url + page).get();
+
+            Elements links = doc.select("a[href]");
+            for (Element link : links){
+                String input = link.text();
+                if(input.matches(".*[A-Z]{4}[0-9]{5}$")){
+                    String code = (input.substring(link.text().length() - 9));
+                    String lk = link.attr("href");
+                    newSB.append(code);
+                    newSB.append(',');
+                    newSB.append(lk);
+                    newSB.append('\n');
+                }
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        return newSB;
+    }
+
+    // gets the last page of the handbook and returns as integer
+    private static int getLastPage(String url){
+        Document doc;
+        Pattern lastPagePattern = Pattern.compile("page=(.*?)$");
+        int last = 1;
+
+        try {
+            doc = Jsoup.connect(url).get();
+            Elements links = doc.select("a[href]");
+            for (Element link : links){
+                String input = link.text();
+                if (input.matches(".*Last.*")){
+                    String l = link.attr("href");
+                    Matcher m = lastPagePattern.matcher(l);
+                    while (m.find()) {
+                        String s1 = m.group(1);
+                        // s1 should now contain the page number as a string
+                        last = Integer.parseInt(s1);
+                    }
+                }
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        return last;
+    }
+}
